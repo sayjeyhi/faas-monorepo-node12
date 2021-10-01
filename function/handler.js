2cd3818b746cfefbfc6865f5b22351b934b8d5a5
sayjeyhi <sayjeyhi@gmail.com>
2021-10-02 00:00:17 +0330

    initial commit

---
+++ function/handler.js
@@ -0,0 +1,10 @@
+module.exports = async (event, context) => {
+  const result = {
+    body: JSON.stringify(event.body),
+    'content-type': event.headers['content-type'],
+  };
+
+  return context
+    .status(200)
+    .succeed(result);
+};
